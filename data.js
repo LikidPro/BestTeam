ca3b70a6eefa5a97d6b7a43c92cde9dabfd9d244
Maria Dominguez <ro.antarez@gmail.com>
2022-10-18 21:04:38 -0300

    data habitaciones

--- data.js
+++ data.js
@@ -1,5 +1,44 @@
-let data = {
-    habitaciones: [
-        
+let dataHabitaciones = {
+    cantidad: 100,
+    habitaciones: [ {
+        id: 1,
+        nombre: "Suite",
+        descripcion: "Espaciosa habitación que se destaca por su gran confort, moderno diseño, particular decoración y equipamiento de alta calidad. La simpleza y refinamiento de los detalles junto al TV LCD, escritorio, sala de estar con sillones y una privilegiada vista al Parque desde el último piso a través de sus amplios ventanales realzan su categoría.La Suite, es la opción ideal para disfrutar del máximo confort en categoría de Habitaciones.",
+        imagen: "https://pxpics.imgix.net/1747/P3490/photos/ad5bf1eceed4ab29742e016c97f85bc5ec64c267.jpg?auto=format&browser=Chrome&h=400&ixlib=php-3.3.1&w=600&s=baa06906412046c1c41bcdd8c5e0f0d8",
+        tamaño: "45 m2",
+        capacidad: 4,
+        comodidades: ["Desayuno Incluido", "Tv", "Wi-fi", "Baño privado", "Aire acondicionado", "calefacción"],
+        cantidad: 30
+    },
+    {
+        id: 2,
+        nombre: "Executive",
+        descripcion: "La antesala con sillones, su cómoda barra desayunadora complementada por un equipamiento específico, su innovador diseño y sus luminosos ventanales con la privilegiada vista al Parque, hacen de este tipo de habitación el elegido por todo aquel que busca comodidad, amplitud y espacio para trabajar distendido, o simplemente relajarse disfrutando la vista.",
+        imagen: "https://pxpics.imgix.net/1747/P3490/photos/e3afab4e4466680f84f6184c3fb02241c175c170.jpg?auto=format&browser=Chrome&h=400&ixlib=php-3.3.1&w=600&s=178a8a43eeeeb914d03fbe8477fcd0bd",
+        tamaño: "32 m2",
+        capacidad: 3,
+        comodidades: ["Desayuno Incluido", "Tv", "Wi-fi", "Baño privado", "Aire acondicionado", "calefacción", "Caja de seguridad"],
+        cantidad: 40
+    },
+    {
+        id: 3,
+        nombre: "Standar",
+        descripcion: "Delicada y confortable habitación que cuida el detalle en su moderna decoración y equipamiento brindando el mejor nivel de calidad. Este tipo de habitación, con 21 m2, es el más grande de su clase en San Rafael, y por lo tanto es ideal para negocios o vacaciones de unos días.",
+        imagen: "https://pxpics.imgix.net/1747/P3490/photos/96775d57616a3a7d4b8d5a1c4d8f582fdd4e39e6.jpg?auto=format&browser=Chrome&h=400&ixlib=php-3.3.1&w=600&s=91da10472e779b4b4d16190ea75fa39d",
+        tamaño: "20 m2",
+        capacidad: 2,
+        comodidades: ["Desayuno Incluido", "Tv", "Wi-fi", "Aire acondicionado", "Caja de seguridad"],
+        cantidad: 30
+    },
+
     ]
 }
+
+
+// {
+//     nombre: "suite",
+//     descripcion: ,
+//     tamaño: ,
+//     capacidad: ,
+//     comodidades: []
+// },
---
+++ index.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            habitaciones: dataHabitaciones.habitaciones
+        }
+    },
+    created() {
+    },
+    mounted() {
+
+    },
+    methods: {
+    },
+    computed: {
+    }
+}).mount('#app')
